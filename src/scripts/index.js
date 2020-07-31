1bf4aaaee55a08d103401c2edcc2c63ecbcec77e
Finn Nils Gedrath <fgedrath@th-koeln.de>
2020-07-31 17:17:53 +0200

    Add Background Support

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,10 @@
 const config = {
     baseURL: 'http://localhost:5500/',
+    singleview: {
+        root: 'singleview',
+        bg: 'background',
+        card: 'singleview__card',
+    },
 };
 
 async function fetchData(apiURL, parseJSON = true) {
@@ -18,34 +23,42 @@ async function fetchData(apiURL, parseJSON = true) {
 
 class SingleView {
     constructor(selector, data, template) {
-        this.root = document.querySelector(selector);
+        this.selector = selector;
+        this.root = document.querySelector(`.${selector.root}`);
+        this.bg = document.querySelector(`.${selector.bg}`);
         this.data = data;
         this.template = template;
         this.lang = 'de';
         this.current = undefined;
+
+        this.init();
+    }
+
+    init() {
+        this.bg.addEventListener('click', (event) => {
+            event.stopPropagation();
+            window.location.hash = '#/';
+        });
     }
 
     open(inventoryNumber) {
         this.current = this.getData(inventoryNumber);
 
-        this.root.classList.remove('singleview-container--visible');
+        this.root.classList.remove(`${this.selector.root}--visible`);
+        this.bg.classList.remove(`${this.selector.bg}--visible`);
 
         if (this.current === undefined) {
             return;
         }
 
-        this.root.classList.add('singleview-container--visible');
+        this.root.classList.add(`${this.selector.root}--visible`);
+        this.bg.classList.add(`${this.selector.bg}--visible`);
 
         this.fillWithData(this.current);
     }
 
     openWithUrl(url) {
         this.open(SingleView.getInventoryNumber(url));
-    }
-
-    close() {
-        // delete all data
-        // remove class
     }
 
     fillWithData(data) {
@@ -70,9 +83,9 @@ class SingleView {
         en: (await fetchData(`${config.baseURL}src/data/cda-paintings-v2.en.json`)).items,
     };
 
-    const template = await fetchData(`${config.baseURL}src/templates/card.mustache.html`, false);
+    const template = await fetchData(`${config.baseURL}src/templates/singleview.mustache.html`, false);
 
-    const singleview = new SingleView('.singleview-container', data, template);
+    const singleview = new SingleView(config.singleview, data, template);
 
     if (window.location.hash === '') {
         window.location.hash = '#/';
