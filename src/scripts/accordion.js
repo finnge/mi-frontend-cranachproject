4cfdc17ca689324acbbbfca3bfaa6cfa94021580
Finn Nils Gedrath <fgedrath@th-koeln.de>
2020-08-01 14:03:05 +0200

    Add Basic Class

---
+++ src/scripts/accordion.js
@@ -0,0 +1,39 @@
+const config = {
+    baseURL: 'http://localhost:5500/',
+    accordion: {
+        root: 'period',
+        header: 'period-header',
+        list: 'period-list',
+        icon: 'period-header__icon',
+    },
+};
+
+class Accordion {
+    constructor(element, selector, state = 'open') {
+        this.selector = selector;
+        this.root = element;
+        this.header = this.root.querySelector(`.${this.selector.header}`);
+        this.list = this.root.querySelector(`.${this.selector.list}`);
+        this.icon = this.root.querySelector(`.${this.selector.icon}`);
+        this.state = state;
+    }
+
+    init() {
+        this.icon.addEventListener('click', () => {
+            if (this.state === 'open') this.close();
+            else if (this.state === 'close') this.open();
+        });
+    }
+
+    open() {
+
+    }
+
+    close() {
+
+    }
+}
+
+(() => {
+
+})();
