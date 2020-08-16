7778e8256572fb14e750caa969b339cdf8a10804
Finn Nils Gedrath <fgedrath@th-koeln.de>
2020-08-16 16:28:13 +0100

    Fixed Race Conditions

--- helper/static-page-generator.js
+++ helper/static-page-generator.js
@@ -10,129 +10,132 @@ const config = {
         data: (file) => `src/data/${file}.json`,
     },
 };
+(async () => {
+    const origData = {
+        de: JSON.parse(fs.readFileSync(config.path.data('cda-paintings-v2.de'))).items,
+        en: JSON.parse(fs.readFileSync(config.path.data('cda-paintings-v2.en'))).items,
+    };
 
-const origData = {
-    de: JSON.parse(fs.readFileSync(config.path.data('cda-paintings-v2.de'))).items,
-    en: JSON.parse(fs.readFileSync(config.path.data('cda-paintings-v2.en'))).items,
-};
+    console.log('✔︎ [1/7] retrieved data');
 
-console.log('✔︎ [1/7] retrieved data');
+    // merging the languages
+    const mergedData = [];
 
-// merging the languages
-const mergedData = [];
+    origData.de.forEach((artefactDE) => {
+        const artefactEN = origData.en.find((el) => el.objectId === artefactDE.objectId);
 
-origData.de.forEach((artefactDE) => {
-    const artefactEN = origData.en.find((el) => el.objectId === artefactDE.objectId);
+        // Create single artefact
+        const artefact = {};
 
-    // Create single artefact
-    const artefact = {};
+        const singleKeys = [
+            'objectName',
+            'objectId',
+            'inventoryNumber',
+            'sortingNumber',
+            'isBestOf',
+            'images',
+        ];
 
-    const singleKeys = [
-        'objectName',
-        'objectId',
-        'inventoryNumber',
-        'sortingNumber',
-        'isBestOf',
-        'images',
-    ];
+        singleKeys.forEach((key) => {
+            artefact[key] = artefactDE[key];
 
-    singleKeys.forEach((key) => {
-        artefact[key] = artefactDE[key];
+            delete artefactDE[key];
+            delete artefactEN[key];
+        });
 
-        delete artefactDE[key];
-        delete artefactEN[key];
+        artefact.sortingNumber = artefact.sortingNumber.split('-');
+
+        artefact.period = artefactDE.dating.begin;
+
+        artefact.de = artefactDE;
+        artefact.en = artefactEN;
+
+        mergedData.push(artefact);
     });
 
-    artefact.sortingNumber = artefact.sortingNumber.split('-');
+    console.log('✔︎ [2/7] merge languages');
 
-    artefact.period = artefactDE.dating.begin;
+    // delete not functioning artefacts
+    await Promise.all(mergedData.map(async (artefact, index) => {
+        try {
+            const res = await fetch(artefact.images.sizes.xs.src);
 
-    artefact.de = artefactDE;
-    artefact.en = artefactEN;
+            console.log(res.ok);
 
-    mergedData.push(artefact);
-});
+            if (!res.ok) {
+                delete mergedData[index];
+            }
+        } catch (e) {
+            delete mergedData[index];
+        }
+    }));
 
-console.log('✔︎ [2/7] merge languages');
+    console.log('✔︎ [3/7] delete not functioning artefacts');
 
-// delete not functioning artefacts
-mergedData.filter(async (artefact) => {
-    try {
-        const res = await fetch(artefact.images.sizes.xs.src);
+    // sort
+    mergedData.sort((a, b) => {
+        let i = 0;
 
-        console.log(res.ok);
+        while (a.sortingNumber[i] !== undefined || b.sortingNumber[i] !== undefined) {
+            if (a.sortingNumber[i] > b.sortingNumber[i]) {
+                return 1;
+            }
+            if (a.sortingNumber[i] < b.sortingNumber[i]) {
+                return -1;
+            }
 
-        return res.ok === false;
-    } catch (e) {
-        return true;
-    }
-});
+            i += 1;
+        }
 
-console.log('✔︎ [3/7] delete not functioning artefacts');
-
-// sort
-mergedData.sort((a, b) => {
-    let i = 0;
-
-    while (a.sortingNumber[i] !== undefined || b.sortingNumber[i] !== undefined) {
-        if (a.sortingNumber[i] > b.sortingNumber[i]) {
+        if (a.sortingNumber[i] === undefined && b.sortingNumber[i] !== undefined) {
             return 1;
         }
-        if (a.sortingNumber[i] < b.sortingNumber[i]) {
+        if (a.sortingNumber[i] !== undefined && b.sortingNumber[i] === undefined) {
             return -1;
         }
+        return 0;
+    });
 
-        i += 1;
-    }
+    console.log('✔︎ [4/7] sort artefacts');
 
-    if (a.sortingNumber[i] === undefined && b.sortingNumber[i] !== undefined) {
-        return 1;
-    }
-    if (a.sortingNumber[i] !== undefined && b.sortingNumber[i] === undefined) {
-        return -1;
-    }
-    return 0;
-});
+    // prev, next
+    mergedData.forEach((artefact, index) => {
+        mergedData[index].prev = (index === 0)
+            ? mergedData[mergedData.length - 1].inventoryNumber
+            : mergedData[index - 1].inventoryNumber;
+        mergedData[index].next = (index === (mergedData.length - 1))
+            ? mergedData[0].inventoryNumber
+            : mergedData[index + 1].inventoryNumber;
+    });
 
-console.log('✔︎ [4/7] sort artefacts');
+    console.log('✔︎ [5/7] group');
 
-// prev, next
-mergedData.forEach((artefact, index) => {
-    mergedData[index].prev = (index === 0)
-        ? mergedData[mergedData.length - 1].inventoryNumber
-        : mergedData[index - 1].inventoryNumber;
-    mergedData[index].next = (index === (mergedData.length - 1))
-        ? mergedData[0].inventoryNumber
-        : mergedData[index + 1].inventoryNumber;
-});
+    // group in periods
+    const periods = [];
+    mergedData.forEach((artefact) => {
+        const periodIndex = periods.findIndex((el) => el.period === artefact.period);
 
-console.log('✔︎ [5/7] group');
+        if (periodIndex < 0) {
+            periods.push({
+                period: artefact.period,
+                artefacts: [artefact],
+            });
+        } else {
+            periods[periodIndex].artefacts.push(artefact);
+        }
+    });
 
-// group in periods
-const periods = [];
-mergedData.forEach((artefact) => {
-    const periodIndex = periods.findIndex((el) => el.period === artefact.period);
+    console.log('✔︎ [6/7] create periods');
 
-    if (periodIndex < 0) {
-        periods.push({
-            period: artefact.period,
-            artefacts: [artefact],
-        });
-    } else {
-        periods[periodIndex].artefacts.push(artefact);
-    }
-});
+    // templating
+    const template = {
+        index: fs.readFileSync(config.path.templates('index')).toString(),
+    };
 
-console.log('✔︎ [6/7] create periods');
+    const output = Mustache.render(template.index, { periods });
 
-// templating
-const template = {
-    index: fs.readFileSync(config.path.templates('index')).toString(),
-};
+    // printing
+    fs.writeFileSync('index.html', output);
 
-const output = Mustache.render(template.index, { periods });
-
-// printing
-fs.writeFileSync('index.html', output);
-
-console.log('✔︎ [7/7] create file');
+    console.log('✔︎ [7/7] create file');
+})();
